56407fdd578cc0683b810aed74a1d123aae9ac4a
Sijan666 <666majharulislam@gmail.com>
2026-02-07 01:44:09 +0600

    add : footer top in root layouts

--- src/components/layouts/RootLayouts.jsx
+++ src/components/layouts/RootLayouts.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./Header";
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
+import FooterTop from "./FooterTop";
 
 const RootLayouts = () => {
   return (
     <>
       <Header />
       <Outlet />
+      <FooterTop/>
       <Footer />
     </>
   );
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -13,7 +13,6 @@ const Home = () => {
         <Category/>
         <FlashDeals/>
         <FeaturedProduct/>
-        <FooterTop/>
         </>
         )
 }
